4f1aa552d8848f5662bad2f2196a2f190b92b4af
lauracb <lauracadavidb@gmail.com>
2019-02-25 15:21:21 -0500

    Funcionalidad de filtrado desde el controlador

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,7 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    @expenses = Expense.order("date DESC")
+    @expenses = params[:concept].present? ? @expenses.where("concept LIKE ?", "%#{params[:concept]}%") : @expenses
+    @expenses = params[:category_id].present? ? @expenses.where(category_id: params[:category_id]) : @expenses
   end
 end
